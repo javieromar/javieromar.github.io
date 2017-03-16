0e59d8ac1f20327a9de3acd04a7a86bffd412c11
javieromar <chocorroloromonlvx@hotmail.com>
2017-03-16 00:57:12 -0600

    se movieron archivos antiguos y de agrego el nuevo index de este parcial

---
+++ script.js
@@ -0,0 +1,12 @@
+/*function tablas(){
+    var i,j
+    var num=prompt("incerte valor")
+    var tab=new int[num][num]
+    for(i=1;i<num+1;i++){
+        for(j=1;j<num+1;j++){
+            tab[i][j]=i*j
+        }
+    }
+    window.open(""'_blank')
+    document.write('texto')
+}*/
